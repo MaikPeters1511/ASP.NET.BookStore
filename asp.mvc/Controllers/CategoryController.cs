50fc1ec05e0ccc5fa21b1a2fb09e3111018e0459
Maiki <bewerbung@maikpeters.org>
2024-10-24 10:11:27 +0200

    Erweitere CategoryController und verbessere Datenanzeige
    - `CategoryController.cs`: `using`-Anweisungen um `asp.mvc.Data` und `asp.mvc.Models` erweitert.
    - Konstruktor von `CategoryController` akzeptiert und initialisiert `ApplicationDbContext`.
    - Neue Methode `GetCategoriesList` hinzugefügt.
    - Methode `Index` geändert, um Kategorienliste an die View zurückzugeben.
    - `ApplicationDbContext.cs`: Konstruktor akzeptiert `DbContextOptions<ApplicationDbContext>`, leerer Konstruktor entfernt.
    - `Index.cshtml`: Modell auf `IEnumerable<Category>` gesetzt, HTML-Tabelle zur Anzeige der Kategorienliste hinzugefügt.

--- asp.mvc/Controllers/CategoryController.cs
+++ asp.mvc/Controllers/CategoryController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using asp.mvc.Data;
+using asp.mvc.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace asp.mvc.Controllers
 {
-    public class CategoryController : Controller
+    public class CategoryController(ApplicationDbContext context) : Controller
     {
+        private readonly ApplicationDbContext context = context;
 
-        public IActionResult Index()
+        public IEnumerable<Category> GetCategoriesList()
         {
-            return View();
+            return context.Categories.ToList();
+        }
+
+        public IActionResult Index(IEnumerable<Category> CategoriesList)
+        {
+            return View(CategoriesList);
         }
     }
 }
