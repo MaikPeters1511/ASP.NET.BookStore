98f9544b590edd3f9364ef7214209b6da836155f
Maiki <bewerbung@maikpeters.org>
2024-11-11 10:32:35 +0100

    CDN-Verweis für knockout.mapping hinzugefügt, Formatierung
    In der Datei `Index.cshtml` wurden die lokalen Skriptverweise auf
    `knockout.mapping` entfernt und durch einen CDN-Verweis auf
    `knockout.mapping` Version 2.4.1 ersetzt. Der Verweis auf `category.js`
    bleibt unverändert.
    
    In der Datei `category.js` wurden einige Formatierungsänderungen
    vorgenommen, um die Konsistenz zu verbessern. Insbesondere wurden
    Semikolons hinzugefügt, um die Zeilen abzuschließen. Die Funktionsweise
    des Codes bleibt unverändert.

--- asp.mvc/wwwroot/js/category.js
+++ asp.mvc/wwwroot/js/category.js
@@ -1,15 +1,15 @@
 ﻿function ViewModel() {
-    let self = this
-    self.categories = ko.observableArray([])
+    let self = this;
+    self.categories = ko.observableArray([]);
 
     self.loadCategories = function () {
         $.getJSON('/Category/GetCategories', function (data) {
-            let mappedCategories = ko.mapping.fromJS(data)
-            self.categories(mappedCategories())
+            let mappedCategories = ko.mapping.fromJS(data);
+            self.categories(mappedCategories());
         });
     };
 
-    self.loadCategories()
+    self.loadCategories();
 }
 
-ko.applyBindings(new ViewModel())
+ko.applyBindings(new ViewModel());
